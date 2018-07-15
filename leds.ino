9f18cef1f586fd8b2507d52cb1a0f05bbd19f90d
spyddy93 <sseba07@gmail.com>
2018-07-15 18:27:09 +0200

    adding arduino cde

---
+++ leds.ino
@@ -0,0 +1,93 @@
+#define LED1 13
+#define LED2 12
+#define LED3 11
+#define LED4 10
+#define LED5 9
+#define LED6 8
+#define LED7 7
+#define LED8 6
+#define LED9 5
+#define LED10 4
+#define LED11 3
+#define LED12 2
+#define LED13 1
+#define LED14 0
+
+void setup() {
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
+  pinMode(LED4,OUTPUT);
+  pinMode(LED5,OUTPUT);
+  pinMode(LED6,OUTPUT);
+  pinMode(LED7,OUTPUT);
+  pinMode(LED8,OUTPUT);
+  pinMode(LED9,OUTPUT);
+  pinMode(LED10,OUTPUT);
+  pinMode(LED11,OUTPUT);
+  pinMode(LED12,OUTPUT);
+  pinMode(LED13,OUTPUT);
+  pinMode(LED14,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED1,HIGH);
+  delay(100);
+  digitalWrite(LED2,HIGH);
+  delay(100);
+  digitalWrite(LED3,HIGH);
+  delay(100);
+  digitalWrite(LED4,HIGH);
+  delay(100);
+  digitalWrite(LED5,HIGH);
+  delay(100);
+  digitalWrite(LED6,HIGH);
+  delay(100);
+  digitalWrite(LED7,HIGH);
+  delay(100);
+  digitalWrite(LED8,HIGH);
+  delay(100);
+  digitalWrite(LED9,HIGH);
+  delay(100);
+  digitalWrite(LED10,HIGH);
+  delay(100);
+  digitalWrite(LED11,HIGH);
+  delay(100);
+  digitalWrite(LED12,HIGH);
+  delay(100);
+  digitalWrite(LED13,HIGH);
+  delay(100);
+  digitalWrite(LED14,HIGH);
+  delay(100);
+
+  digitalWrite(LED1,LOW);
+  delay(100);
+  digitalWrite(LED2,LOW);
+  delay(100);
+  digitalWrite(LED3,LOW);
+  delay(100);
+  digitalWrite(LED4,LOW);
+  delay(100);
+  digitalWrite(LED5,LOW);
+  delay(100);
+  digitalWrite(LED6,LOW);
+  delay(100);
+  digitalWrite(LED7,LOW);
+  delay(100);
+  digitalWrite(LED8,LOW);
+  delay(100);
+  digitalWrite(LED9,LOW);
+  delay(100);
+  digitalWrite(LED10,LOW);
+  delay(100);
+  digitalWrite(LED11,LOW);
+  delay(100);
+  digitalWrite(LED12,LOW);
+  delay(100);
+  digitalWrite(LED13,LOW);
+  delay(100);
+  digitalWrite(LED14,LOW);
+  delay(100);
+
+}
